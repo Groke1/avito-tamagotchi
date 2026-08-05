fa12d60b6b8a55aa6377550cefe512b0b7dafef4
Aleksandr Fomin <rightclickoff@gmail.com>
2026-08-05 14:42:51 +0700

    Feat/issue 20 get create pet (#35)
    * feat: init pets service
    
    * feat: implemented happy path
    
    * feat: implemented all paths
    
    * feat: added .env and fixed status code
    
    * fix: changed route path
    
    ---------
    
    Co-authored-by: Alexander <a.fomin1@g.nsu.ru>

---
+++ backend/pets/cmd/main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/cayman444/avito-gamification-hackathon/blob/main/backend/pets/internal/api"
+	"github.com/cayman444/avito-gamification-hackathon/blob/main/backend/pets/internal/repository"
+	"github.com/cayman444/avito-gamification-hackathon/blob/main/backend/pets/internal/service"
+	"github.com/joho/godotenv"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Printf(".env not found: %v", err)
+	}
+
+	dsn := os.Getenv("DATABASE_URL")
+	if dsn == "" {
+		log.Fatal("DATABASE_URL is not set")
+		return
+	}
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		log.Fatal("PORT is not set")
+	}
+
+	authSecret := os.Getenv("AUTH_SECRET")
+	if authSecret == "" {
+		log.Fatal("AUTH_SECRET is not set")
+	}
+
+	db, err := sqlx.Connect("postgres", dsn)
+	if err != nil {
+		log.Fatalf("Database connection failed: %v", err)
+	}
+	defer db.Close()
+
+	repository := repository.NewPetRepository(db)
+	service := service.NewPetService(repository)
+	handler := api.NewPetHandler(service)
+
+	r := chi.NewRouter()
+
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
+	r.Route("/api/v1/pet", func(r chi.Router) {
+		r.Use(api.AuthMiddleware(authSecret))
+		r.Get("/", handler.GetPet)
+		r.Post("/", handler.CreatePet)
+	})
+
+	log.Println("[SERVICE STARTED]")
+	http.ListenAndServe(":"+port, r)
+}
---
+++ backend/pets/migrations/000001_create_pets.up.sql
@@ -0,0 +1,14 @@
+CREATE TABLE IF NOT EXISTS pets (
+    id BIGSERIAL PRIMARY KEY,
+    user_id BIGINT NOT NULL UNIQUE,
+    name VARCHAR(32) NOT NULL,
+    level INT NOT NULL DEFAULT 1,
+    xp INT NOT NULL DEFAULT 0,
+    next_level_xp INT NOT NULL DEFAULT 100,
+    satiety INT NOT NULL DEFAULT 60,
+    happiness INT NOT NULL DEFAULT 75,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+);
+
+CREATE INDEX IF NOT EXISTS idx_pets_user_id 
+ON pets(user_id);
