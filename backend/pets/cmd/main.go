7cdf1b70a4a014fdbf77c6c62ab3e018b34ff8b8
Groke <409458@niuitmo.ru>
2026-08-07 22:31:28 +0300

    fix: fix cors

--- backend/pets/cmd/main.go
+++ backend/pets/cmd/main.go
@@ -4,11 +4,11 @@ import (
 	"log"
 	"net/http"
 
+	cors "github.com/cayman444/avito-gamification-hackathon.pkg/middleware"
 	"github.com/cayman444/avito-gamification-hackathon/backend/pets/internal/api"
 	"github.com/cayman444/avito-gamification-hackathon/backend/pets/internal/config"
 	"github.com/cayman444/avito-gamification-hackathon/backend/pets/internal/repository"
 	"github.com/cayman444/avito-gamification-hackathon/backend/pets/internal/service"
-
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	"github.com/jmoiron/sqlx"
@@ -40,7 +40,7 @@ func main() {
 	r.Use(middleware.Recoverer)
 
 	r.Route("/api/v1", func(r chi.Router) {
-		r.Use(api.CorsMiddleware)
+		r.Use(cors.CorsHandler)
 		r.Use(api.JwtMiddleware(cfg.JwtSecret))
 
 		r.Route("/pet", func(r chi.Router) {
