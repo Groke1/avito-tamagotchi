8c0d01ed123d629aaeaf054eb5773645ed434c38
Groke <409458@niuitmo.ru>
2026-08-08 05:05:30 +0300

    feat: send streak to pet

---
+++ backend/user/internal/adapter/pets/http/pets.go
@@ -0,0 +1,61 @@
+package http
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"net/http"
+	"net/url"
+	"time"
+
+	"github.com/cayman444/avito-gamification-hackathon.user/internal/adapter/httpclient"
+)
+
+type client struct {
+	baseURL    *url.URL
+	httpClient *http.Client
+}
+
+func NewPetClient(addr string, httpClient *http.Client) (*client, error) {
+	addr = httpclient.NormalizeAddr(addr)
+	if addr == "" {
+		return nil, errors.New("pet service address is empty")
+	}
+
+	baseURL, err := url.Parse(addr)
+	if err != nil {
+		return nil, fmt.Errorf("parse pet service address: %w", err)
+	}
+
+	if httpClient == nil {
+		httpClient = &http.Client{
+			Timeout: 2 * time.Second,
+		}
+	}
+
+	return &client{
+		baseURL:    baseURL,
+		httpClient: httpClient,
+	}, nil
+}
+
+func (c *client) SendDailyBonus(ctx context.Context, userID string, streak int32) error {
+	payload := dailyBonusRequest{
+		UserID: userID,
+		Streak: streak,
+	}
+
+	endpoint := c.baseURL.ResolveReference(
+		&url.URL{
+			Path: "/internal/daily-bonus",
+		},
+	)
+
+	_, err := httpclient.PostJSON[dailyBonusRequest, emptyResponse](
+		ctx, c.httpClient, endpoint.String(), payload)
+	if err != nil {
+		return fmt.Errorf("send daily bonus: %w", err)
+	}
+
+	return nil
+}
