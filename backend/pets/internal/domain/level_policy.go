b5af59847dda1ee402395b155a1cfd81c62579e0
Alexander <a.fomin1@g.nsu.ru>
2026-08-09 14:22:54 +0700

    feat: intermediate commit

---
+++ backend/pets/internal/domain/level_policy.go
@@ -0,0 +1,18 @@
+package domain
+
+type LevelPolicy struct {
+	configs map[int]string
+}
+
+func NewLevelPolicy() *LevelPolicy {
+	return &LevelPolicy{
+		configs: map[int]string{
+			1: "",
+			2: "DELIVERY_DISCOUNT_10",
+			3: "LISTING_DISCOUNT_15",
+			4: "AUTOTEKA_DISCOUNT_20",
+			5: "FREE_LISTING_HIGHLIGHT",
+			6: "FREE_LISTING_PROMOTION",
+		},
+	}
+}
