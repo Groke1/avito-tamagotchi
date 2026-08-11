1ad10cb60b544c3c78ce666248fcf06b6d4583b2
Ulyana <shirokovaulyanochka@yandex.ru>
2026-08-11 21:14:50 +0300

    fix: bug in GetUserTaskForUpdate

--- backend/tasks/internal/postgres/db/query.sql
+++ backend/tasks/internal/postgres/db/query.sql
@@ -57,7 +57,6 @@ FROM random_tasks;
 SELECT
     ut.status,
     ut.completed_at,
-
     t.id,
     t.title,
     t.description,
@@ -68,6 +67,8 @@ FROM user_tasks ut
 JOIN tasks t ON t.id = ut.task_id
 WHERE ut.user_id = $1
   AND ut.task_id = $2
+  AND ut.updated_at >= CURRENT_DATE AND 
+ut.updated_at < CURRENT_DATE + INTERVAL '1 day'
 FOR UPDATE OF ut;
 
 -- name: UpdateUserTaskCompleted :exec
