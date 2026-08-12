26aa134cff4ac1c867d09ed319159b83da307aec
Артур Савин <tyrchikx@gmail.com>
2026-08-12 14:31:51 +0300

    feat: add PetHungry widget to the dashboard page

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,10 +1,12 @@
 import { HeaderDashboard } from '@/widgets/header'
 import { PetShowcase, TodayQuestsWidget } from '@/widgets/pet-dashboard'
+import { PetHungry } from '@/widgets/pet-hungry'
 
 export const Dashboard = () => {
   return (
     <div className="h-full space-y-6">
       <HeaderDashboard />
+      <PetHungry />
       <div className="grid grid-cols-1 lg:grid-cols-10 gap-6 ">
         <div className="lg:col-span-5">
           <PetShowcase />
